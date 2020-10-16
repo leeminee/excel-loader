491d84ce497be28ad7ef456540b625a170228f0e
이민이 <mllee@ipageon.com>
2020-10-16 17:09:17 +0900

    jxls 셀값 넣는것 찾아보기

--- src/main/java/com/wendy/excelloader/model/Alarm.java
+++ src/main/java/com/wendy/excelloader/model/Alarm.java
@@ -2,8 +2,23 @@ package com.wendy.excelloader.model;
 
 public class Alarm {
 
+    private String alarmHost;
     private String alarmMessage;
     private int alarmCount;
+
+    public Alarm(String alarmHost, String alarmMessage, int alarmCount) {
+        this.alarmHost = alarmHost;
+        this.alarmMessage = alarmMessage;
+        this.alarmCount = alarmCount;
+    }
+
+    public String getAlarmHost() {
+        return alarmHost;
+    }
+
+    public void setAlarmHost(String alarmHost) {
+        this.alarmHost = alarmHost;
+    }
 
     public String getAlarmMessage() {
         return alarmMessage;
--- src/main/java/com/wendy/excelloader/service/ExcelServiceImpl.java
+++ src/main/java/com/wendy/excelloader/service/ExcelServiceImpl.java
@@ -1,12 +1,31 @@
 package com.wendy.excelloader.service;
 
+import com.wendy.excelloader.ExcelLoaderApplication;
+import com.wendy.excelloader.model.Alarm;
+import net.sf.jxls.transformer.XLSTransformer;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class ExcelServiceImpl implements ExcelService{
 
     @Override
     public String createExcel() {
+
+        HashMap<String, Object> map = new HashMap<>();
+        String excelPath = ExcelLoaderApplication.class.getClassLoader().getResource("excel/excel-sample.xlsx").toString();
+        System.out.println("excelPath = " + excelPath);
+
+        XLSTransformer xlsTransformer = new XLSTransformer();
+
+        List<Alarm> alarmHostList = new ArrayList<>();
+
+        alarmHostList.add(new Alarm("message",1));
+
+
         return "hello";
     }
 }
